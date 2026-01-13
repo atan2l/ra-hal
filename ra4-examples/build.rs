d489d5ea74430c08225fd90cc445e680aa2ee840
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-12 19:29:14 -0800

    Fix the no-defmt case.

--- ra4-examples/build.rs
+++ ra4-examples/build.rs
@@ -6,6 +6,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
+
+    #[cfg(feature = "defmt")]
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
 
     let gitcl = GitclBuilder::default().all().sha(true).build()?;
