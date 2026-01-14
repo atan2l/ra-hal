c9d3ea4a93994ebc670cfeda00813f0c52301171
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-13 16:11:58 -0800

    Further blink cleanup.

--- ra4-examples/src/bin/blink.rs
+++ ra4-examples/src/bin/blink.rs
@@ -49,6 +49,74 @@ pub static SEC_MPU: [u32; 13] = [
     0x407ffffc, 0x407fffff, 0x400dfffc, 0x400dffff, 0xffffffff,
 ];
 
+fn print_clock_config(config: Sckdivcr) {
+    let hoco_freq = 48;
+    let ick_freq = match config.ick() {
+        Ick::DIV_1 => hoco_freq,
+        Ick::DIV_2 => hoco_freq / 2,
+        Ick::DIV_4 => hoco_freq / 4,
+        Ick::DIV_8 => hoco_freq / 8,
+        Ick::DIV_16 => hoco_freq / 16,
+        Ick::DIV_32 => hoco_freq / 32,
+        Ick::DIV_64 => hoco_freq / 64,
+        Ick::_RESERVED_7 => unimplemented!("Invalid sckdivcr.ick"),
+    };
+    let fck_freq = match config.fck() {
+        Fck::DIV_1 => hoco_freq,
+        Fck::DIV_2 => hoco_freq / 2,
+        Fck::DIV_4 => hoco_freq / 4,
+        Fck::DIV_8 => hoco_freq / 8,
+        Fck::DIV_16 => hoco_freq / 16,
+        Fck::DIV_32 => hoco_freq / 32,
+        Fck::DIV_64 => hoco_freq / 64,
+        Fck::_RESERVED_7 => unimplemented!("Invalid sckdivcr.fck"),
+    };
+    let pck_a = match config.pcka() {
+        Pcka::DIV_1 => hoco_freq,
+        Pcka::DIV_2 => hoco_freq / 2,
+        Pcka::DIV_4 => hoco_freq / 4,
+        Pcka::DIV_8 => hoco_freq / 8,
+        Pcka::DIV_16 => hoco_freq / 16,
+        Pcka::DIV_32 => hoco_freq / 32,
+        Pcka::DIV_64 => hoco_freq / 64,
+        Pcka::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pcka"),
+    };
+    let pck_b = match config.pckb() {
+        Pckb::DIV_1 => hoco_freq,
+        Pckb::DIV_2 => hoco_freq / 2,
+        Pckb::DIV_4 => hoco_freq / 4,
+        Pckb::DIV_8 => hoco_freq / 8,
+        Pckb::DIV_16 => hoco_freq / 16,
+        Pckb::DIV_32 => hoco_freq / 32,
+        Pckb::DIV_64 => hoco_freq / 64,
+        Pckb::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pckb"),
+    };
+    let pck_c = match config.pckc() {
+        Pckc::DIV_1 => hoco_freq,
+        Pckc::DIV_2 => hoco_freq / 2,
+        Pckc::DIV_4 => hoco_freq / 4,
+        Pckc::DIV_8 => hoco_freq / 8,
+        Pckc::DIV_16 => hoco_freq / 16,
+        Pckc::DIV_32 => hoco_freq / 32,
+        Pckc::DIV_64 => hoco_freq / 64,
+        Pckc::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pckc"),
+    };
+    let pck_d = match config.pckd() {
+        Pckd::DIV_1 => hoco_freq,
+        Pckd::DIV_2 => hoco_freq / 2,
+        Pckd::DIV_4 => hoco_freq / 4,
+        Pckd::DIV_8 => hoco_freq / 8,
+        Pckd::DIV_16 => hoco_freq / 16,
+        Pckd::DIV_32 => hoco_freq / 32,
+        Pckd::DIV_64 => hoco_freq / 64,
+        Pckd::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pckd"),
+    };
+    debug!(
+        "ICK: {} MHz, FCK: {} MHz, PCKA: {} MHz, PCKB: {} MHz, PCKC: {} MHz, PCKD: {} MHz",
+        ick_freq, fck_freq, pck_a, pck_b, pck_c, pck_d
+    );
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Starting board init");
@@ -137,77 +205,12 @@ async fn main(_spawner: Spawner) {
         w.set_pcka(Pcka::DIV_1);
     });
 
-    let clock_divs = system.sckdivcr().read();
-    let hoco_freq = 48;
-    let ick_freq = match clock_divs.ick() {
-        Ick::DIV_1 => hoco_freq,
-        Ick::DIV_2 => hoco_freq / 2,
-        Ick::DIV_4 => hoco_freq / 4,
-        Ick::DIV_8 => hoco_freq / 8,
-        Ick::DIV_16 => hoco_freq / 16,
-        Ick::DIV_32 => hoco_freq / 32,
-        Ick::DIV_64 => hoco_freq / 64,
-        Ick::_RESERVED_7 => unimplemented!("Invalid sckdivcr.ick"),
-    };
-    let fck_freq = match clock_divs.fck() {
-        Fck::DIV_1 => hoco_freq,
-        Fck::DIV_2 => hoco_freq / 2,
-        Fck::DIV_4 => hoco_freq / 4,
-        Fck::DIV_8 => hoco_freq / 8,
-        Fck::DIV_16 => hoco_freq / 16,
-        Fck::DIV_32 => hoco_freq / 32,
-        Fck::DIV_64 => hoco_freq / 64,
-        Fck::_RESERVED_7 => unimplemented!("Invalid sckdivcr.fck"),
-    };
-    let pck_a = match clock_divs.pcka() {
-        Pcka::DIV_1 => hoco_freq,
-        Pcka::DIV_2 => hoco_freq / 2,
-        Pcka::DIV_4 => hoco_freq / 4,
-        Pcka::DIV_8 => hoco_freq / 8,
-        Pcka::DIV_16 => hoco_freq / 16,
-        Pcka::DIV_32 => hoco_freq / 32,
-        Pcka::DIV_64 => hoco_freq / 64,
-        Pcka::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pcka"),
-    };
-    let pck_b = match clock_divs.pckb() {
-        Pckb::DIV_1 => hoco_freq,
-        Pckb::DIV_2 => hoco_freq / 2,
-        Pckb::DIV_4 => hoco_freq / 4,
-        Pckb::DIV_8 => hoco_freq / 8,
-        Pckb::DIV_16 => hoco_freq / 16,
-        Pckb::DIV_32 => hoco_freq / 32,
-        Pckb::DIV_64 => hoco_freq / 64,
-        Pckb::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pckb"),
-    };
-    let pck_c = match clock_divs.pckc() {
-        Pckc::DIV_1 => hoco_freq,
-        Pckc::DIV_2 => hoco_freq / 2,
-        Pckc::DIV_4 => hoco_freq / 4,
-        Pckc::DIV_8 => hoco_freq / 8,
-        Pckc::DIV_16 => hoco_freq / 16,
-        Pckc::DIV_32 => hoco_freq / 32,
-        Pckc::DIV_64 => hoco_freq / 64,
-        Pckc::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pckc"),
-    };
-    let pck_d = match clock_divs.pckd() {
-        Pckd::DIV_1 => hoco_freq,
-        Pckd::DIV_2 => hoco_freq / 2,
-        Pckd::DIV_4 => hoco_freq / 4,
-        Pckd::DIV_8 => hoco_freq / 8,
-        Pckd::DIV_16 => hoco_freq / 16,
-        Pckd::DIV_32 => hoco_freq / 32,
-        Pckd::DIV_64 => hoco_freq / 64,
-        Pckd::_RESERVED_7 => unimplemented!("Invalid sckdivcr.pckd"),
-    };
-    debug!(
-        "ICK: {} MHz, FCK: {} MHz, PCKA: {} MHz, PCKB: {} MHz, PCKC: {} MHz, PCKD: {} MHz",
-        ick_freq, fck_freq, pck_a, pck_b, pck_c, pck_d
-    );
-
     system.prcr().write(|w| {
         w.set_prkey(crate::pac::system::vals::Prkey::PROTECT_KEY);
         w.set_prc0(Prc0::Protected);
     });
+
+    print_clock_config(system.sckdivcr().read());
 
     info!("Finished board init");
 
