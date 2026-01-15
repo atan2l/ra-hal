eea62567238f15d6a4f67f691a88cedbac36c37d
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-15 01:24:10 -0800

    Flesh out a nicer FMIFRT interface.

--- ra4-hal/src/lib.rs
+++ ra4-hal/src/lib.rs
@@ -2,6 +2,7 @@
 
 pub mod adc;
 pub mod crc;
+pub mod mcu_info;
 pub mod ofs0;
 pub mod ofs1;
 pub mod sce5;
@@ -10,7 +11,6 @@ pub mod time_driver;
 pub mod uart;
 pub mod write_protect;
 
-use cfg_if::cfg_if;
 use cortex_m::asm;
 #[allow(unused)]
 use defmt::{debug, error, info, trace, warn};
@@ -23,7 +23,7 @@ use ra4m1_ctpac::{
     system::vals::{Cksel, Fck, Hcfrq1, Hcstp, Ick, Opcm, Pcka, Pckb, Pckc, Pckd},
 };
 
-use crate::write_protect::WriteProtect as _;
+use crate::{mcu_info::McuInfo, write_protect::WriteProtect as _};
 
 pub fn init() -> Peripherals {
     // #define BSP_CLOCK_CFG_MAIN_OSC_WAIT (9)
@@ -42,7 +42,7 @@ pub fn init() -> Peripherals {
             fmifrt_base.base().read().base()
         );
 
-        print_mcu_info();
+        McuInfo::info().print_info();
 
         let system = pac::SYSTEM;
 
@@ -116,98 +116,6 @@ pub fn init() -> Peripherals {
 
         p
     })
-}
-
-pub fn print_mcu_info() {
-    let fmifrt = pac::FMIFRT;
-
-    let uid: [u32; 4] = [
-        fmifrt.uidr(0).read().uid(),
-        fmifrt.uidr(1).read().uid(),
-        fmifrt.uidr(2).read().uid(),
-        fmifrt.uidr(3).read().uid(),
-    ];
-
-    let mut part_number: [u8; 16] = [0; 16];
-
-    part_number[0..4].copy_from_slice(&fmifrt.pnr(0).read().0.to_ne_bytes());
-    part_number[4..8].copy_from_slice(&fmifrt.pnr(1).read().0.to_ne_bytes());
-    part_number[8..12].copy_from_slice(&fmifrt.pnr(2).read().0.to_ne_bytes());
-    part_number[12..16].copy_from_slice(&fmifrt.pnr(3).read().0.to_ne_bytes());
-
-    let pn = core::str::from_utf8(&part_number).unwrap().trim();
-    let ver = fmifrt.mcuver().read().mcuver();
-
-    const PN_LEN: usize = 13;
-
-    if pn.len() < PN_LEN {
-        info!(
-            "MCU: {} rev {:02X}, UID: {:08x}-{:08x}-{:08x}-{:08x}",
-            pn, ver, uid[0], uid[1], uid[2], uid[3]
-        );
-        warn!("PN too short to identify");
-    } else {
-        let flash_size = match part_number[8] {
-            b'9' => Some(128),
-            b'B' => Some(256),
-            b'C' => Some(384),
-            b'D' => Some(512),
-            b'E' => Some(768),
-            b'F' => Some(1024),
-            _ => todo!(),
-        };
-
-        // Check if the crate was configured correctly
-        let pin_count = match &part_number[11..=12] {
-            b"FB" | b"BM" => Some(144),
-            b"FP" | b"LJ" => Some(100),
-            b"NB" | b"BQ" | b"BB" | b"FM" => Some(64),
-            b"NG" => Some(56),
-            b"NE" | b"FL" => Some(48),
-            b"NF" => Some(40),
-            b"BC" => Some(36),
-            b"NH" | b"FJ" => Some(32),
-            suffix => {
-                warn!("Unknown suffix: {}", suffix);
-                None
-            }
-        };
-
-        info!(
-            "MCU: {} rev {:02X}, flash={} KB, UID: {:08x}-{:08x}-{:08x}-{:08x}",
-            pn,
-            ver,
-            flash_size.unwrap_or(0),
-            uid[0],
-            uid[1],
-            uid[2],
-            uid[3]
-        );
-
-        match pin_count {
-            Some(actual) => {
-                cfg_if! {
-                    if #[cfg(feature = "100pin")] {
-                        let configured = 100;
-                    } else if #[cfg(feature = "64pin")] {
-                        let configured = 64;
-                    } else if #[cfg(feature = "48pin")] {
-                        let configured = 48;
-                    } else {
-                        let configured = 40;
-                    }
-                }
-
-                if configured != actual {
-                    warn!(
-                        "May not behave as expected. HAL configured with {} pins, MCU has {} pins",
-                        configured, actual
-                    );
-                }
-            }
-            None => warn!("Couldn't determine appropriate pin count"),
-        }
-    }
 }
 
 pub fn print_clock_config() {
---
+++ ra4-hal/src/mcu_info.rs
@@ -0,0 +1,152 @@
+//! `FMIFRT` Factory MCU Information Flash Root Table
+//!
+//! This is all read only so we don't care all that much about ownership of the peripheral.
+
+use core::mem::transmute;
+
+use cfg_if::cfg_if;
+#[allow(unused)]
+use defmt::{debug, error, info, trace, warn};
+
+use crate::pac;
+
+/// Contains information about the MCU that was programmed at the factory.
+pub struct McuInfo {
+    uid: u128,
+    part_number: [u8; 16],
+    rev: u8,
+}
+
+impl McuInfo {
+    const PN_LEN: usize = 13;
+
+    /// Returns the part number as a string.
+    pub fn part_number(&self) -> &str {
+        core::str::from_utf8(&self.part_number).unwrap().trim()
+    }
+
+    /// Returns the unique 128-bit identifier associated with the MCU.
+    #[inline]
+    pub fn uid(&self) -> u128 {
+        self.uid
+    }
+
+    /// Returns the revision of the MCU.  Higher values are newer.
+    #[inline]
+    pub fn revision(&self) -> u8 {
+        self.rev
+    }
+
+    /// Returns `true` if the part number was log enough to be parsed.
+    /// If `false` is returned neither `flash_size` nor `pin_count` will work.
+    pub fn ok(&self) -> bool {
+        self.part_number().len() >= Self::PN_LEN
+    }
+
+    /// Returns the amount of on-die flash memory in kilobytes
+    pub fn flash_size(&self) -> Option<u16> {
+        match self.part_number[8] {
+            b'9' => Some(128),
+            b'B' => Some(256),
+            b'C' => Some(384),
+            b'D' => Some(512),
+            b'E' => Some(768),
+            b'F' => Some(1024),
+            _ => todo!(),
+        }
+    }
+
+    pub fn pin_count(&self) -> Option<u8> {
+        match &self.part_number[11..=12] {
+            b"FB" | b"BM" => Some(144),
+            b"FP" | b"LJ" => Some(100),
+            b"NB" | b"BQ" | b"BB" | b"FM" => Some(64),
+            b"NG" => Some(56),
+            b"NE" | b"FL" => Some(48),
+            b"NF" => Some(40),
+            b"BC" => Some(36),
+            b"NH" | b"FJ" => Some(32),
+            suffix => {
+                warn!("Unknown suffix: {}", suffix);
+                None
+            }
+        }
+    }
+
+    /// Writes MCU information to the logger
+    pub fn print_info(&self) {
+        if !self.ok() {
+            info!(
+                "MCU: {} rev {:02X}, UID: {=u128:032x}",
+                self.part_number(),
+                self.revision(),
+                self.uid(),
+            );
+            warn!("PN too short to identify");
+        } else {
+            // Check if the crate was configured correctly
+
+            info!(
+                "MCU: {} rev {:02X}, flash={} KB, UID: {=u128:032x}",
+                self.part_number(),
+                self.revision(),
+                self.flash_size().unwrap_or(0),
+                self.uid(),
+            );
+
+            match self.pin_count() {
+                Some(actual) => {
+                    cfg_if! {
+                        if #[cfg(feature = "100pin")] {
+                            let configured = 100;
+                        } else if #[cfg(feature = "64pin")] {
+                            let configured = 64;
+                        } else if #[cfg(feature = "48pin")] {
+                            let configured = 48;
+                        } else {
+                            let configured = 40;
+                        }
+                    }
+
+                    if configured != actual {
+                        warn!(
+                            "May not behave as expected. HAL configured with {} pins, MCU has {} pins",
+                            configured, actual
+                        );
+                    }
+                }
+                None => warn!("Couldn't determine appropriate pin count"),
+            }
+        }
+    }
+
+    /// Loads information from `FMIFRT`.
+    pub fn info() -> Self {
+        let fmifrt = pac::FMIFRT;
+
+        let uid: [u32; 4] = [
+            fmifrt.uidr(3).read().uid(),
+            fmifrt.uidr(2).read().uid(),
+            fmifrt.uidr(1).read().uid(),
+            fmifrt.uidr(0).read().uid(),
+        ];
+
+        // Safety: alignment is less strict here, right?
+        let uid: [u8; 16] = unsafe { transmute(uid) };
+        let uid = u128::from_ne_bytes(uid);
+
+        let mut part_number: [u8; 16] = [0; 16];
+        part_number[0..4].copy_from_slice(&fmifrt.pnr(0).read().0.to_ne_bytes());
+        part_number[4..8].copy_from_slice(&fmifrt.pnr(1).read().0.to_ne_bytes());
+        part_number[8..12].copy_from_slice(&fmifrt.pnr(2).read().0.to_ne_bytes());
+        part_number[12..16].copy_from_slice(&fmifrt.pnr(3).read().0.to_ne_bytes());
+
+        let rev = fmifrt.mcuver().read().mcuver();
+
+        McuInfo {
+            uid,
+            part_number,
+            rev,
+        }
+    }
+}
