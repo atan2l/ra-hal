0996c5d2769c305f11a0291e6a6f34f598743b13
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-19 03:07:30 -0800

    More I2C churn.

--- ra4-hal/src/i2c.rs
+++ ra4-hal/src/i2c.rs
@@ -1,6 +1,9 @@
 use core::marker::PhantomData;
 
-use crate::{gpio::AnyPin, pac};
+use crate::{
+    gpio::{AnyPin, PeripheralFunction, Pin},
+    pac,
+};
 
 use embassy_hal_internal::{Peri, PeripheralType};
 use embedded_hal_1::i2c::{SevenBitAddress, TenBitAddress};
@@ -23,13 +26,59 @@ pub struct DataPin<'d, I: SealedInstance> {
 }
 
 #[allow(private_bounds)]
-pub trait Instance: SealedInstance + PeripheralType + 'static + Send {
-    // type Interrupt: interrupt::typelevel::Interrupt;
-    // const INSTANCE: &str;
-}
+pub trait Instance: SealedInstance + PeripheralType + 'static + Send {}
 
 trait SealedInstance {
-    // const PERIPHERAL_ID: Interrupt;
-
     fn regs() -> pac::iic::Iic;
 }
+
+trait ClockPinSealed<I: SealedInstance>: Pin + PeripheralType {}
+
+trait DataPinSealed<I: SealedInstance>: Pin {}
+
+impl SealedInstance for crate::peripherals::IIC0 {
+    #[inline(always)]
+    fn regs() -> pac::iic::Iic {
+        crate::pac::IIC0
+    }
+}
+
+macro_rules! clock_pin_impl {
+    ($iic:ident, $pin:ident) => {
+        impl crate::i2c::ClockPinSealed<crate::peripherals::$iic> for crate::peripherals::$pin {}
+    };
+}
+
+#[allow(private_bounds)]
+impl<'d, I: SealedInstance> DataPin<'d, I> {
+    /// Takes a pin and configures it to be used by a PWM channel
+    pub fn new(pin: Peri<'d, impl DataPinSealed<I>>) -> Self {
+        pin.set_peripheral_func(PeripheralFunction::I2c);
+
+        Self {
+            pin: pin.into(),
+            _phantom_i: PhantomData,
+        }
+    }
+}
+
+impl<'d, I: SealedInstance> From<DataPin<'d, I>> for AnyPin {
+    fn from(value: DataPin<I>) -> Self {
+        *value.pin
+    }
+}
+
+macro_rules! data_pin_impl {
+    ($iic:ident, $pin:ident) => {
+        impl crate::i2c::DataPinSealed<crate::peripherals::$iic> for crate::peripherals::$pin {}
+    };
+}
+
+// data_pin_impl!(IIC1, P101);
+// data_pin_impl!(IIC1, P206);
+
+data_pin_impl!(IIC0, P401);
+data_pin_impl!(IIC0, P407);
+clock_pin_impl!(IIC0, P204);
+clock_pin_impl!(IIC0, P400);
+clock_pin_impl!(IIC0, P408);
