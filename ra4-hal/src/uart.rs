d152675bc84f170c0d89bf004a78eb3ee2cbfd5a
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-19 19:57:44 -0800

    Start working down generic UART impl.

--- ra4-hal/src/uart.rs
+++ ra4-hal/src/uart.rs
@@ -2,12 +2,16 @@ use core::marker::PhantomData;
 
 use cortex_m::asm;
 use embassy_hal_internal::{Peri, PeripheralType};
+use paste::paste;
 use ra4m1_ctpac::sci0::{
-    regs::{Smr, Tdr},
-    vals::{SmrCks, SmrPm, Stop},
+    regs::{Scr, Tdr},
+    vals::{ScrCke, SmrCks, SmrPm, Stop},
 };
 
-use crate::{pac, peripherals};
+use crate::{
+    gpio::{AnyPin, Flex, PeripheralFunction, Pin},
+    pac, peripherals,
+};
 
 #[allow(private_bounds)]
 pub struct Uart<'d, I: Instance> {
@@ -23,110 +27,315 @@ trait SealedInstance {
     fn stop();
 }
 
-impl Instance for peripherals::SCI0 {}
-impl SealedInstance for peripherals::SCI0 {
-    fn regs() -> ra4m1_ctpac::sci0::Sci0 {
-        crate::pac::SCI0
-    }
+trait TxPinSealed<I: SealedInstance>: Pin + PeripheralType {
+    const PERIPHERAL_FUNC: crate::gpio::PeripheralFunction;
 
-    fn start() {
-        debug!("SCI0: stop=false");
-
-        pac::MSTP.mstpcrb().write(|w| {
-            w.set_mstpb31(false);
-        });
-    }
-
-    fn stop() {
-        debug!("SCI0: stop=true");
-
-        pac::MSTP.mstpcrb().write(|w| {
-            w.set_mstpb31(true);
-        });
+    #[inline(always)]
+    fn pfunc(&self) -> crate::gpio::PeripheralFunction {
+        Self::PERIPHERAL_FUNC
     }
 }
 
+trait RxPinSealed<I: SealedInstance>: Pin + PeripheralType {
+    const PERIPHERAL_FUNC: crate::gpio::PeripheralFunction;
+
+    #[inline(always)]
+    fn pfunc(&self) -> crate::gpio::PeripheralFunction {
+        Self::PERIPHERAL_FUNC
+    }
+}
+
+#[allow(private_bounds)]
+pub struct TxPin<'d, I: SealedInstance> {
+    pin: Peri<'d, AnyPin>,
+    _phantom_i: PhantomData<I>,
+}
+
+#[allow(private_bounds)]
+pub struct RxPin<'d, I: SealedInstance> {
+    pin: Peri<'d, AnyPin>,
+    _phantom_i: PhantomData<I>,
+}
+
+#[allow(private_bounds)]
+impl<'d, I: SealedInstance> TxPin<'d, I> {
+    /// Takes a pin and configures it to be used as an SCI TX line
+    pub fn new(pin: Peri<'d, impl TxPinSealed<I>>) -> Self {
+        pin.set_peripheral_func(pin.pfunc());
+
+        Self {
+            pin: pin.into(),
+            _phantom_i: PhantomData,
+        }
+    }
+}
+
+#[allow(private_bounds)]
+impl<'d, I: SealedInstance> RxPin<'d, I> {
+    /// Takes a pin and configures it to be used as an SCI RX line
+    pub fn new(pin: Peri<'d, impl RxPinSealed<I>>) -> Self {
+        pin.set_peripheral_func(pin.pfunc());
+
+        Self {
+            pin: pin.into(),
+            _phantom_i: PhantomData,
+        }
+    }
+}
+
+macro_rules! tx_pin_impl {
+    ($sci:ident, $pin:ident, $pfunc:ident) => {
+        impl crate::uart::TxPinSealed<crate::peripherals::$sci> for crate::peripherals::$pin {
+            const PERIPHERAL_FUNC: crate::gpio::PeripheralFunction =
+                crate::gpio::PeripheralFunction::$pfunc;
+        }
+    };
+}
+
+macro_rules! rx_pin_impl {
+    ($sci:ident, $pin:ident, $pfunc:ident) => {
+        impl crate::uart::RxPinSealed<crate::peripherals::$sci> for crate::peripherals::$pin {
+            const PERIPHERAL_FUNC: crate::gpio::PeripheralFunction =
+                crate::gpio::PeripheralFunction::$pfunc;
+        }
+    };
+}
+
+macro_rules! instance_impl {
+    ($periph:ident, $stop:ident) => {
+        impl Instance for crate::peripherals::$periph {}
+        paste! {
+            impl SealedInstance for crate::peripherals::$periph {
+                fn regs() -> ra4m1_ctpac::sci0::Sci0 {
+                    crate::pac::$periph
+                }
+
+                fn start() {
+                    debug!("{}: stop=false", stringify!($periph));
+
+                    pac::MSTP.mstpcrb().write(|w| {
+                        w.[< set_ $stop >](false);
+                    });
+                }
+
+                fn stop() {
+                    debug!("{}: stop=true", stringify!($periph));
+
+                    pac::MSTP.mstpcrb().write(|w| {
+                        w.[< set_ $stop >](true);
+                    });
+                }
+            }
+        }
+    };
+}
+
+tx_pin_impl!(SCI0, P101, Sci1);
+tx_pin_impl!(SCI0, P205, Sci1);
+#[cfg(any(feature = "_64pin", feature = "_100pin"))]
+tx_pin_impl!(SCI0, P411, Sci1);
+
+tx_pin_impl!(SCI1, P213, Sci2);
+#[cfg(any(feature = "_64pin", feature = "_100pin"))]
+tx_pin_impl!(SCI1, P401, Sci2);
+#[cfg(any(feature = "_64pin", feature = "_100pin"))]
+tx_pin_impl!(SCI1, P501, Sci2);
+
+rx_pin_impl!(SCI0, P100, Sci1);
+#[cfg(any(feature = "_48pin", feature = "_64pin", feature = "_100pin"))]
+rx_pin_impl!(SCI0, P104, Sci1);
+#[cfg(any(feature = "_48pin", feature = "_64pin", feature = "_100pin"))]
+rx_pin_impl!(SCI0, P206, Sci1);
+#[cfg(any(feature = "_64pin", feature = "_100pin"))]
+rx_pin_impl!(SCI0, P410, Sci1);
+
+rx_pin_impl!(SCI1, P212, Sci2);
+#[cfg(any(feature = "_64pin", feature = "_100pin"))]
+rx_pin_impl!(SCI1, P402, Sci2);
+#[cfg(any(feature = "_64pin", feature = "_100pin"))]
+rx_pin_impl!(SCI1, P502, Sci2);
+#[cfg(feature = "_100pin")]
+rx_pin_impl!(SCI1, P708, Sci2);
+
+instance_impl!(SCI0, mstpb31);
+instance_impl!(SCI1, mstpb30);
+
 impl<'d, I: Instance> Uart<'d, I> {
-    pub fn new(_peri: Peri<'d, I>) -> Self {
+    #[inline]
+    fn set_data_bits(n: u8) {
+        let sci = I::regs();
+
+        match n {
+            7 => {
+                // // 7 Data bits
+                // // Restrictions apply, page 704 note 3
+                sci.scmr().modify(|w| w.set_chr1(true));
+                sci.smr().modify(|w| w.set_chr(true));
+            }
+            8 => {
+                // 8 Data bits
+                info!("Setting 8 data bits");
+                sci.scmr().modify(|w| w.set_chr1(true));
+                sci.smr().modify(|w| w.set_chr(false));
+            }
+            9 => {
+                // // 9 Data bits
+                // sci.scmr().write(|w| w.set_chr1(false));
+                // sci.smr().write(|w| w.set_chr(false));
+                todo!()
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn new(
+        _peri: Peri<'d, I>,
+        tx: Peri<'d, peripherals::P501>,
+        rx: Peri<'d, peripherals::P502>,
+    ) -> Self {
         I::start();
 
         let sci = I::regs();
 
-        // not-smart card mode
-        sci.scmr().write(|w| {
-            w.set_smif(false);
-        });
+        sci.scr().write_value(Scr(0));
 
-        // // 9 Data bits
-        // sci.scmr().write(|w| w.set_chr1(false));
-        // sci.smr().write(|w| w.set_chr(false));
-
-        // 8 Data bits
-        sci.scmr().write(|w| w.set_chr1(true));
-        sci.smr().write(|w| w.set_chr(false));
-
-        // // 7 Data bits
-        // // Restrictions apply, page 704 note 3
-        // sci.scmr().write(|w| w.set_chr1(true));
-        // sci.smr().write(|w| w.set_chr(true));
-
-        // Set it up for _N1
-        sci.smr().write(|w| {
-            w.set_pe(false);
-            // w.set_pm(SmrPm::Even);
-            w.set_stop(Stop::Stop1);
-        });
-
-        sci.smr().write(|w| {
-            w.set_mp(false);
-            w.set_cm(false);
-        });
-
-        sci.scr().write(|w| {
-            w.set_re(false);
+        sci.scr().modify(|w| {
+            w.set_tie(false);
+            w.set_rie(false);
             w.set_te(false);
+            w.set_re(false);
+            w.set_teie(false);
         });
 
-        //Baud=9600 SmallN=0 BigN=156 Error=0.16%
-
-        sci.brr().write(|w| {
-            w.set_brr(156);
-        });
-
-        sci.smr().write(|w| {
-            w.set_cks(SmrCks::DIV_1);
-        });
-
-        sci.semr().write(|w| {
-            w.set_bgdm(false);
-            w.set_abcs(false);
-            w.set_abcse(false);
-        });
-
-        sci.fcr().write(|w| {
+        sci.fcr().modify(|w| {
             // turn off FIFO for now
             w.set_fm(false);
         });
 
-        sci.scr().write(|w| {
+        sci.scr().modify(|w| {
+            w.set_cke(ScrCke::_00);
+        });
+
+        sci.simr1().modify(|w| {
+            w.set_iicm(false);
+        });
+
+        sci.spmr().modify(|w| {
+            w.set_ckph(false);
+            w.set_ckpol(false);
+        });
+
+        // not-smart card mode
+        sci.scmr().modify(|w| {
+            w.set_smif(false);
+        });
+
+        Self::set_data_bits(8);
+
+        // Set it up for No Parity, 1 stop bit
+        sci.smr().modify(|w| {
+            w.set_pe(false);
+            w.set_pm(SmrPm::Even);
+            w.set_stop(Stop::Stop1);
+        });
+
+        sci.smr().modify(|w| {
+            w.set_mp(false);
+            w.set_cm(false);
+        });
+        sci.smr().modify(|w| {
+            w.set_cks(SmrCks::DIV_1);
+        });
+
+        sci.semr().modify(|w| {
+            w.set_brme(true);
+            w.set_bgdm(true);
+            w.set_abcs(false);
+            w.set_abcse(false);
+            w.set_rxdesel(true);
+        });
+
+        sci.sptr().write(|w| {
+            w.set_spb2dt(false);
+            w.set_spb2io(false);
+        });
+
+        // Baud=9600 SmallN=1 BigN=38 Error=0.16%
+        // Baud=19200 SmallN=0 BigN=77 Error=0.16%
+        // Baud=115200 SmallN=0 BigN=12 Error=0.16%
+        sci.brr().write(|w| {
+            w.set_brr(22);
+        });
+
+        sci.mddr().write(|w| {
+            w.set_mddr(226);
+        });
+
+        // Move pins over to SCI1
+        let mut tx = Flex::new(tx);
+        let mut rx = Flex::new(rx);
+        tx.set_peripheral_func(PeripheralFunction::Sci2);
+        rx.set_peripheral_func(PeripheralFunction::Sci2);
+
+        sci.scr().modify(|w| {
             w.set_re(true);
             w.set_te(true);
+            w.set_rie(true);
         });
+
+        warn!("SMR: {}", sci.smr().read());
+        warn!("SCR: {}", sci.scr().read());
+        warn!("SSR: {}", sci.ssr().read());
+        warn!("SEMR: {}", sci.semr().read());
+        warn!("BRR: {}", sci.brr().read());
+        warn!("FCR: {}", sci.fcr().read());
 
         Self {
             _phantom: PhantomData,
         }
     }
 
-    pub fn write(&mut self, data: &[u8]) {
+    pub fn blocking_read(&self, data: &mut [u8]) {
         let sci = I::regs();
+
+        warn!("Uhh? {:02x}", sci.ssr().read());
+        // sci.scr().modify(|w| {
+        //     w.set_te(false);
+        // });
+
+        for byte in data.iter_mut() {
+            while !sci.ssr().read().rdrf() {
+                // error!("waiting");
+                asm::nop();
+            }
+            *byte = sci.rdr().read().rdr();
+        }
+        // sci.scr().modify(|w| {
+        //     w.set_te(true);
+        // });
+    }
+
+    pub fn blocking_write(&mut self, data: &[u8]) {
+        let sci = I::regs();
+
+        // sci.scr().modify(|w| {
+        //     w.set_te(true);
+        // });
 
         for byte in data.iter() {
             while !sci.ssr().read().tdre() {
                 asm::nop();
             }
+            info!("Writing: {:02x}", *byte as char);
             sci.tdr().write_value(Tdr(*byte));
+            while !sci.ssr().read().tdre() {
+                asm::nop();
+            }
         }
+        // sci.scr().modify(|w| {
+        //     w.set_te(false);
+        //     w.set_re(true);
+        // });
     }
 }
 
