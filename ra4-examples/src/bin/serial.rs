b816dfd3961440947d4d7b427e746a6becde26c8
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-23 20:26:56 -0800

    Update the serial example.

--- ra4-examples/src/bin/serial.rs
+++ ra4-examples/src/bin/serial.rs
@@ -1,13 +1,18 @@
 //! Serial demonstrates SCI comms
+//!
+//! This writes AT commands to the onboard ESP32-S3 using different
+//! APIs and checks for a result.
 
 #![no_std]
 #![no_main]
 #![warn(missing_docs)]
 
+use cortex_m::asm;
 #[cfg(feature = "defmt")]
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_time::Timer;
+use embedded_io_async::{Read, Write};
 use panic_probe as _;
 use ra4_hal::{
     bind_interrupts,
@@ -68,7 +73,6 @@ fn query<
     let mut buf = [0_u8; 64];
 
     loop {
-        // warn!("Reading");
         let line_len = uart.read_line(&mut buf);
         let line = &buf[..line_len];
         let line = str::from_utf8(line).unwrap();
@@ -85,22 +89,55 @@ async fn main(_spawner: Spawner) {
 
     print_clock_config();
 
-    let tx_buf = &mut [0u8; 128];
-    let rx_buf = &mut [0u8; 128];
+    // Using small buffers to ensure we test the multiple iterations  path
+    let tx_buf = &mut [0u8; 8];
+    let rx_buf = &mut [0u8; 8];
 
     let mut uart = Uart::new(p.SCI1, p.P501, p.P502, Irqs);
     uart.init_buffers(tx_buf, rx_buf);
 
+    // The ESP32 communicates at 115,200 baud with the default Arduino firmware
     uart.set_speed(115200);
 
+    // Try a command with multiple small blocking writes
     query(&mut uart, b"SOFTRESETWIFI");
     Timer::after_millis(125).await;
-    query(&mut uart, b"FWVERSION?");
-    query(&mut uart, b"GETSTATUS?");
+
+    // Try a command with one large blocking write
+    info!("Sending SOFTRESETWIFI");
+    uart.blocking_write(b"AT+SOFTRESETWIFI\r\n");
+    let mut in_buf = [0_u8; 22];
+    uart.read_exact(&mut in_buf).await.unwrap();
+    let s = str::from_utf8(&in_buf).unwrap();
+    warn!("{:?}", s);
+
+    // Clear the RX buffer just in case.
+    uart.drain();
+
+    // Write this with the async API
+    info!("Sending ASYNC SOFTRESETWIFI");
+    uart.write(b"AT+SOFTRESETWIFI\r\n").await.unwrap();
+    info!("Reading?");
+    let mut in_buf = [0_u8; 22];
+    uart.read_exact(&mut in_buf).await.unwrap();
+    let s = str::from_utf8(&in_buf).unwrap();
+    warn!("{:?}", s);
+
+    uart.drain();
+
+    // Lots o data to read over a long time.
+    info!("WiFi scan");
     query(&mut uart, b"WIFISCAN");
-    query(&mut uart, b"GETSTATUS?");
+
+    // And make sure we still work
+    info!("Sending FWVERSION");
+    uart.write(b"AT+FWVERSION?\r\n").await.unwrap();
+    let mut in_buf = [0_u8; 23];
+    uart.read_exact(&mut in_buf).await.unwrap();
+    let s = str::from_utf8(&in_buf).unwrap();
+    warn!("{:?}", s);
 
     loop {
-        Timer::after_millis(250 * 2).await;
+        asm::nop();
     }
 }
