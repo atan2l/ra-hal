756a267aa0a3268424c16484b1eff46d2edd33dc
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-01-23 16:23:07 -0800

    GC some additional PFS registers and fields.

--- ra4m1-ctpac/src/pfs.rs
+++ ra4m1-ctpac/src/pfs.rs
@@ -20,66 +20,6 @@ impl Pfs {
         assert!(n < 144usize);
         unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x0usize + n * 4usize) as _) }
     }
-    #[doc = "P108 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p108pfs(self) -> crate::common::Reg<regs::P108pfs, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x60usize) as _) }
-    }
-    #[doc = "P108 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p108pfs_ha(self) -> crate::common::Reg<regs::P108pfsHa, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x62usize) as _) }
-    }
-    #[doc = "P108 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p108pfs_by(self) -> crate::common::Reg<regs::P108pfsBy, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x63usize) as _) }
-    }
-    #[doc = "P109 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p109pfs(self) -> crate::common::Reg<regs::P109pfs, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x64usize) as _) }
-    }
-    #[doc = "P109 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p109pfs_ha(self) -> crate::common::Reg<regs::P109pfsHa, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x66usize) as _) }
-    }
-    #[doc = "P109 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p109pfs_by(self) -> crate::common::Reg<regs::P109pfsBy, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x67usize) as _) }
-    }
-    #[doc = "P201 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p201pfs(self) -> crate::common::Reg<regs::P201pfs, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x84usize) as _) }
-    }
-    #[doc = "P201 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p201pfs_ha(self) -> crate::common::Reg<regs::P201pfsHa, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x86usize) as _) }
-    }
-    #[doc = "P201 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p201pfs_by(self) -> crate::common::Reg<regs::P201pfsBy, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x87usize) as _) }
-    }
-    #[doc = "P408 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p408pfs(self) -> crate::common::Reg<regs::P408pfs, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x0120usize) as _) }
-    }
-    #[doc = "P408 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p408pfs_ha(self) -> crate::common::Reg<regs::P408pfsHa, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x0122usize) as _) }
-    }
-    #[doc = "P408 Pin Function Control Register"]
-    #[inline(always)]
-    pub const fn p408pfs_by(self) -> crate::common::Reg<regs::P408pfsBy, crate::common::RW> {
-        unsafe { crate::common::Reg::from_ptr(self.ptr.wrapping_add(0x0123usize) as _) }
-    }
 }
 pub mod regs;
 pub mod vals;
