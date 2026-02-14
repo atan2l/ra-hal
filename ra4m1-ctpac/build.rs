53bd84e9d9eb14d9de32b201e937f44504d18f17
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-02-14 00:35:15 -0800

    ra4m1-ctpac: Get build.rs working w/ cargo publish

--- ra4m1-ctpac/build.rs
+++ ra4m1-ctpac/build.rs
@@ -1,24 +1,27 @@
 use std::{env, fs::File, io::Write, path::PathBuf};
 
-fn add_linker_script(input_path: &str, output_path: &str) {
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+fn add_linker_script(in_filename: &str, out_filename: &str) {
+    let out_dir = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
-    let input_data = std::fs::read(input_path).unwrap();
+    let input_data = std::fs::read(in_filename)
+        .unwrap_or_else(|e| panic!("Couldn't read: {in_filename}\n{e:#?}"));
 
-    File::create(out.join(output_path))
+    let out_path = out_dir.join(out_filename);
+
+    File::create(&out_path)
         .unwrap()
         .write_all(&input_data)
-        .unwrap();
+        .unwrap_or_else(|e| panic!("Couldn't write: {out_path:?}\n{e:#?}"));
 
-    println!("cargo:rustc-link-search={}", out.display());
-    println!("cargo:rerun-if-changed={}", input_path);
+    println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rerun-if-changed={}", in_filename);
 }
 
 fn main() {
     if env::var_os("CARGO_FEATURE_RT").is_some() {
         add_linker_script("device.x", "ra-device.x");
     }
-    add_linker_script("../support/ra-link.x", "ra-link.x");
+    add_linker_script("ra-link.x", "ra-link.x");
 
     println!("cargo:rerun-if-changed=build.rs");
 }
