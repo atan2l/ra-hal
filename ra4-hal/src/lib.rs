2c8ecaa50d6c898e17c6836889fc71e05d528700
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-02-15 23:11:31 -0800

    Add a precautionary tale.

--- ra4-hal/src/lib.rs
+++ ra4-hal/src/lib.rs
@@ -10,6 +10,11 @@
 pub mod fmt;
 
 pub mod adc;
+// Yep, we're gating CAN behind a 48 MHz main clock (ICLK).
+// W… w… why?
+// CAN requires PCLKA : PCLKB = 2:1 (§30.9.2) and with ICLK != 48 MHz you're looking at
+// PCLKB = 16 MHz which seems unnecessarily slow.
+#[cfg(feature = "_hoco_48mhz")]
 pub mod can;
 pub mod crc;
 pub mod dac;
