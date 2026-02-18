5d641b45a5f77815e9a76ed5f84824094908775a
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-02-17 18:46:24 -0800

    uart: Add entries for HOCO=64 MHz

--- ra4-hal/src/uart.rs
+++ ra4-hal/src/uart.rs
@@ -228,7 +228,7 @@ const SPEED_ENTRIES: [SpeedEntry; 8] = [
     SpeedEntry { baud: 115200, small_n: 0, big_n: 12,  modulation: 0 },
 ];
 
-#[cfg(feature = "_hoco_32mhz")]
+#[cfg(any(feature = "_hoco_32mhz", feature = "_hoco_64mhz"))]
 #[rustfmt::skip]
 const SPEED_ENTRIES: [SpeedEntry; 7] = [
     SpeedEntry { baud: 300,    small_n: 3, big_n: 52,  modulation: 0 },
