9afd469c2639d3aa44bc01a695e4de98b14949bb
Alex Zepeda <srht@inferiorhumanorgans.com>
2026-05-19 03:26:06 -0700

    Rudimentary DTC error handling.

--- examples/ra4m1/src/bin/dac.rs
+++ examples/ra4m1/src/bin/dac.rs
@@ -136,7 +136,7 @@ async fn main(_spawner: Spawner) {
             trigger.fire();
 
             // Wait for transfer to finish
-            transfer.await;
+            transfer.await.expect("DTC transfer failed");
 
             // Wait a bit to stretch out the waveform
             while now.elapsed() < Duration::from_nanos(250_000) {
